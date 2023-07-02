14fc0ee165784b5582238f2fc030ad66d6d5ff11
xss9149 <wi22b099@technikum-wien.at>
2023-07-02 07:49:49 +0200

    Initial commit

---
+++ src/test/java/com/example/MaintenanceMonitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MaintenanceMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
